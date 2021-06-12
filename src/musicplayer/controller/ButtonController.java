5414b3917981b145c1e27d5d1e82700a9aed19ce
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2021-06-12 22:21:57 +0800

    [Controller]Add ButtonController Class

---
+++ src/musicplayer/controller/ButtonController.java
@@ -0,0 +1,44 @@
+package musicplayer.controller;
+
+import musicplayer.model.Playlist;
+import musicplayer.model.Song;
+
+public class ButtonController {
+    
+    public void addButtonClicked() {
+        
+    }
+    
+    public void sortButtonClicked() {
+        
+    }
+    
+    public void searchButtonClicked() {
+        
+    }
+    
+    public void deleteButtonClicked() {
+        
+    }
+    
+    public void firstButtonClicked() {
+        
+    }
+    
+    public void backButtonClicked() {
+        
+    }
+    
+    public void playButtonClicked() {
+        
+    }
+    
+    public void nextButtonClicked() {
+        
+    }
+    
+    public void lastButtonClicked() {
+        
+    }
+    
+}
